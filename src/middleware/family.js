f942c6091f1c32018c9a7c1a285491b1724576bf
roman <rcecile@reseau.free.fr>
2021-09-19 18:59:32 +0200

    remove check

--- src/middleware/family.js
+++ src/middleware/family.js
@@ -30,6 +30,10 @@ const familyAPI = (store) => (next) => (action) => {
         }
       )
         .then((response) => {
+          localStorage.setItem("userId", response.data.family[0].user.id);
+          store.dispatch(
+            stateUsers("login", response.data.family, response.data.sid)
+          );
           store.dispatch(axiosCheckLog());
         })
         .catch((err) => {
